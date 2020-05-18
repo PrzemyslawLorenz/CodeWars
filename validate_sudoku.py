60feeae4c6c82c8174657ee8b0625bd889f1a71a
unknown <przemoleko@gmail.com>
2020-05-18 20:30:44 +0200

    Next task (not ready)

---
+++ validate_sudoku.py
@@ -0,0 +1,53 @@
+import math
+import pprint
+
+
+class Sudoku(object):
+    def __init__(self, data):
+        self.all = []
+        for x in range(len(data)):
+            newColumn = []
+            self.all.append(data[x])
+            for y in range(len(data)):
+                newColumn.append(data[y][x])
+            self.all.append(newColumn)
+
+    def is_valid(self):
+        pprint.pprint(self.all)
+        for i in self.all:
+            for j in i:
+                if i.count(j) > 1:
+                    return False
+        return True
+
+
+goodSudoku1 = Sudoku([
+    [7, 8, 4, 1, 5, 9, 3, 2, 6],
+    [5, 3, 9, 6, 7, 2, 8, 4, 1],
+    [6, 1, 2, 4, 3, 8, 7, 5, 9],
+
+    [9, 2, 8, 7, 1, 5, 4, 6, 3],
+    [3, 5, 7, 8, 4, 6, 1, 9, 2],
+    [4, 6, 1, 9, 2, 3, 5, 8, 7],
+
+    [8, 7, 6, 3, 9, 4, 2, 1, 5],
+    [2, 4, 3, 5, 6, 1, 9, 7, 8],
+    [1, 9, 5, 2, 8, 7, 6, 3, 4]
+])
+
+badSudoku1 = Sudoku([
+    [0, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+])
+
+print(goodSudoku1.is_valid())
+print(badSudoku1.is_valid())
