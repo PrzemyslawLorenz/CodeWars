de0e6e2304cb5d549ee45ce26866311abb0eb411
unknown <przemoleko@gmail.com>
2020-05-21 09:08:57 +0200

    Completed task

--- make_a_spiral.py
+++ make_a_spiral.py
@@ -1,26 +1,28 @@
-import pprint
-
-
-def spiral(size):
+def spiralize(size):
     if size < 5:
         return 0
-    macierz = []
+    spiral = []
     x = size
     y = 0
     for i in range(size):
-        macierz.append(['0'] * size)
+        spiral.append([0] * size)
     while x > 0:
         for i in reversed(range(y, x)):
-            for j in reversed(range(x)):
-                if i == x - 1 or i == y or j == x - 1 or j == 0:
-                    macierz[i][j] = '1'
-                if j == 0 and i == 1:
-                    macierz[i][j] = '0'
+            for j in reversed(range(y-1, x)):
+                if i == y or j == x - 1 or j == y:
+                    spiral[i][j] = 1
+                if i == x - 1 and j > y:
+                    spiral[i][j] = 1
+                if i == y and j == y-1:
+                    spiral[i][j] = 1
+                if j == y and i == y+1:
+                    spiral[i][j] = 0
         x -= 2
         y += 2
-    pprint.pprint(macierz)
+
+    return spiral
 
 
-print(spiral(5))
-print(spiral(10))
-print(spiral(9))
+print(spiralize(5))
+print(spiralize(10))
+print(spiralize(9))
