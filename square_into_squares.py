1e09c814ccb1d479faf3878b2d4d94932dffde6d
unknown <przemoleko@gmail.com>
2020-05-23 19:06:30 +0200

    Completed task

--- square_into_squares.py
+++ square_into_squares.py
@@ -3,6 +3,7 @@ def decompose(number):
     array = []
     summ = []
     x = number
+    y = -1
 
     while sum(summ) != number**2:
         for i in reversed(range(1, x)):
@@ -13,12 +14,16 @@ def decompose(number):
 
         if sum(summ) != number**2:
             try:
-                x = array[1]
+                if array[y] == 1:
+                    x = array[y-1]
+                    y -= 1
+                else:
+                    x = array[y]
             except IndexError:
                 return None
-            target = number**2 - array[0]**2
-            del(array[1:])
-            del(summ[1:])
+            del(array[y:])
+            del(summ[y:])
+            target = number**2 - sum(summ)
 
     return sorted(array)
 
